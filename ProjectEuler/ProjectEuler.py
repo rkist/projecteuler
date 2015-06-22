81392621e44415ce2535271d5bbf58d0a76f1dbb
rkist <raulkist@gmail.com>
2015-06-22 20:19:20 +0200

    exer 2 done

--- ProjectEuler/ProjectEuler.py
+++ ProjectEuler/ProjectEuler.py
@@ -1,5 +1,17 @@
 import module1
 import module2
 
+fibArray = module2.BuildFibonacciSequenceMaximumBased(4000000)
 
-print module1.GetSumOfMultiplesInRange(3,0,1000)
+sum1 = module2.GetSumOfNonMultiplesInArray(1,fibArray)
+sum2 = module2.GetSumOfNonMultiplesInArray(2,fibArray)
+
+sum3 = module2.GetSumOfMultiplesInArray(1,fibArray)
+sum4 = module2.GetSumOfMultiplesInArray(2,fibArray)
+
+
+print fibArray
+print sum1
+print sum2
+print sum3
+print sum4
--- ProjectEuler/module2.py
+++ ProjectEuler/module2.py
@@ -1,3 +1,43 @@
-#def FiconacciRecursion():
+def GetSumOfMultiplesInArray(multipleOf, values):
+    sum = 0
+    for x in values:
+        if x % multipleOf == 0:
+            sum += x
+    return sum
 
-#def Fibonacci(max):
+def GetSumOfNonMultiplesInArray(multipleOf, array):
+    sum = 0
+    for x in array:
+        if x % multipleOf != 0:
+            sum += x
+    return sum
+
+def FibonacciRecursion(index):
+    if index == 0:
+        return 1
+    if index == 1:
+        return 2
+    return FibonacciRecursion(index-1) + FibonacciRecursion(index-2)
+
+def BuildFibonacciSequenceIndexBased(maxIndex):
+    arr = []
+    for index in range(0,maxIndex):
+        arr.append(FibonacciRecursion(index))
+
+    return arr
+
+def BuildFibonacciSequenceMaximumBased(maxValue):
+    arr = []
+    currVal = 0
+    index = 0
+    while True:
+        currVal = FibonacciRecursion(index)
+        index+=1
+        if currVal < maxValue:
+            arr.append(currVal)
+        else:
+            return arr
+
+        
+
+    
