61d43c0e8aa1a7b950f341e8536d0fbc8f420073
rkist <raulkist@gmail.com>
2015-06-23 10:56:50 +0200

    doing ex5

--- ProjectEuler/ProjectEuler.py
+++ ProjectEuler/ProjectEuler.py
@@ -2,17 +2,7 @@ import module1
 import module2
 import module3
 import module4
+import module5
 
-
-
-
-isPal = module4.IsPalindrome(12345)
-print isPal
-
-isPal = module4.IsPalindrome(12321)
-print isPal
-
-isPal = module4.IsPalindrome(123321)
-print isPal
 
 
--- ProjectEuler/module4.py
+++ ProjectEuler/module4.py
@@ -7,3 +7,15 @@ def IsPalindrome(number):
         if numStr[index] != numStr[-(index+1)]:
             return False
     return True
+
+def GetMaxPalindromeFromMultiplOfNumInRange(start,end):
+    maxPal = 0
+    for i0 in range(start,end):
+        for i1 in range(start,end):
+            num = i0*i1
+            isPal = IsPalindrome(num)
+            if isPal:
+                if num > maxPal:
+                    maxPal = num
+                    #print maxPal
+    return maxPal
