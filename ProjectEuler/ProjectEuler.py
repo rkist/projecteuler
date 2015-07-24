490cda2c2a65bbb4c542cd5dfe42ef736ffa3b25
Raul Kist <raulkist@gmail.com>
2015-07-24 19:57:36 +0200

    doing ex14

--- ProjectEuler/ProjectEuler.py
+++ ProjectEuler/ProjectEuler.py
@@ -1,7 +1,7 @@
 import module14
 
 
-seq = module14.GetCollatzSequence(1)
+seq = module14.GetCollatzSequence(13)
 
 print seq
 
--- ProjectEuler/module14.py
+++ ProjectEuler/module14.py
@@ -1,2 +1,14 @@
 def GetCollatzSequence(n):
-    return 1
+    seq = [n]
+    GetCollatzSequenceRecursion(seq)
+    return seq
+
+def GetCollatzSequenceRecursion(seq):
+    last = seq[-1]
+    if last == 1:
+        return seq
+    elif last % 2 == 0: 
+        seq.append(last/2)
+    else:
+        seq.append(3*last+1)
+    return GetCollatzSequenceRecursion(seq)
