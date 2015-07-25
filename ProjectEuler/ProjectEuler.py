491e1ec484fffa2550a213b06867f15ee93f34c9
Raul Kist <raulkist@gmail.com>
2015-07-25 12:20:26 +0200

    dynamic

--- ProjectEuler/ProjectEuler.py
+++ ProjectEuler/ProjectEuler.py
@@ -1,9 +1,11 @@
 from module15 import *
 
-n1 = FindNumberOfPaths((0,0), (2,2))
-n2 = FindNumberOfPathsDynamic((0,0), (2,2))
+#n = FindNumberOfPaths((0,0), (10,10))
+#print n
 
-print n1, n2
+n = FindNumberOfPathsDynamic((0,0), (10,10))
+print n
+
 
 
 
--- ProjectEuler/module15.py
+++ ProjectEuler/module15.py
@@ -8,15 +8,22 @@ def FindNumberOfPaths(start, end):
     else:
         return FindNumberOfPaths((start[0],start[1]+1),end) + FindNumberOfPaths((start[0]+1,start[1]),end)
 
-
-
 def FindNumberOfPathsDynamic(start, end):
+    pathmatrix = [[-1 for x in xrange(start[0], end[0]+1)] for x in xrange(start[1], end[1]+1)]
+    #print len(pathmatrix), len(pathmatrix[0])
+    return __FindNumberOfPathsDynamicRecursion__(start, end, pathmatrix)
+
+
+
+def __FindNumberOfPathsDynamicRecursion__(start, end, pathmatrix):
+    if pathmatrix[start[0]][start[1]] != -1:
+        return pathmatrix[start[0]][start[1]]
     if start == end:
         return 1
     if start[0] == end[0]:
-        return FindNumberOfPaths((start[0],start[1]+1),end)
+        pathmatrix[start[0]][start[1]] = FindNumberOfPaths((start[0],start[1]+1),end)
     if start[1] == end[1]:
-        return FindNumberOfPaths((start[0]+1,start[1]),end)
+        pathmatrix[start[0]][start[1]] = FindNumberOfPaths((start[0]+1,start[1]),end)
     else:
-        return FindNumberOfPaths((start[0],start[1]+1),end) + FindNumberOfPaths((start[0]+1,start[1]),end)
-
+        pathmatrix[start[0]][start[1]] = FindNumberOfPaths((start[0],start[1]+1),end) + FindNumberOfPaths((start[0]+1,start[1]),end)
+    return pathmatrix[start[0]][start[1]]
