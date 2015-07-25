a5fda388f78f3aa9c996def876f9262b7c37510e
Raul Kist <raulkist@gmail.com>
2015-07-25 12:02:48 +0200

    doing ex14

--- ProjectEuler/ProjectEuler.py
+++ ProjectEuler/ProjectEuler.py
@@ -1,22 +1,10 @@
-import module14
+from module15 import *
 
 
-#seq = module14.GetCollatzSequence(13)
-#print len(seq)
-#print seq
+n = FindNumberOfPaths((0,0), (20,20))
 
+print n
 
-
-
-maxSize = -1
-maxN = 1
-
-for n in xrange(1,1000000):
-    size = module14.GetCollatzSequenceSize(n)
-    if size > maxSize:
-        maxSize = size
-        maxN = n
-        print maxN, maxSize
 
 
 
---
+++ ProjectEuler/module15.py
@@ -0,0 +1,10 @@
+def FindNumberOfPaths(start, end):
+    if start == end:
+        return 1
+    if start[0] == end[0]:
+        return FindNumberOfPaths((start[0],start[1]+1),end)
+    if start[1] == end[1]:
+        return FindNumberOfPaths((start[0]+1,start[1]),end)
+    else:
+        return FindNumberOfPaths((start[0],start[1]+1),end) + FindNumberOfPaths((start[0]+1,start[1]),end)
+
