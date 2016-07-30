0518164b28e3ba7b343dc4d22e2791526dd09b13
Raul Kist <raulkist@gmail.com>
2016-07-30 02:25:19 +0200

    24 solved

--- ProjectEuler/module24.py
+++ ProjectEuler/module24.py
@@ -1,4 +1,33 @@
 
+def Permute(string):
+    stringToAdd = ""
+    stringToRemove = string
+    permutations = []
+    for i in range(0,len(stringToRemove)):
+        PermuteRecursion(i, stringToAdd, stringToRemove, permutations)
+
+    return permutations
+
+
+def PermuteRecursion(index, stringToAdd, stringToRemove, permutations):
+
+    newStringToAdd = stringToAdd + stringToRemove[index]
+    newStringToRemove = stringToRemove[:index] + stringToRemove[index+1:]
+
+    if (len(newStringToRemove) == 0):
+        permutations.append(newStringToAdd)
+        return
+
+    for i in range(0,len(newStringToRemove)):
+        PermuteRecursion(i, newStringToAdd, newStringToRemove, permutations)
+
 
 def SolveEx24():
-    print "ok"
+
+    str = "0123456789"
+
+    perms = Permute(str)
+
+    print perms[1000000-1]
+
+
