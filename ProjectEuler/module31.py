fb3042142f05a322e75ab04d66749c003d146c0a
Raul Kist <Raul.Kist@cegal.com>
2016-08-11 10:40:20 +0200

    31 started

--- ProjectEuler/module31.py
+++ ProjectEuler/module31.py
@@ -1,5 +1,15 @@
+import math
 
+#possible coins: 1p, 2p, 5p, 10p, 20p, 50p, 1 (100p) and 2 (200p).
+AllPossibleCoins = {200, 100, 50, 20, 10, 5, 2, 1}
+
+CoinsCombinationsArray = []
+
+
+def CombinateCoins(chosenCoins, remainingCoins, remainingAmount):
+
+  pass
 
 
 def SolveProblem():
-    pass
+  pass
