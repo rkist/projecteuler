8d9a978be858e3e68b6c3ff3c74686928afb3a90
Raul Kist <raulkist@gmail.com>
2016-08-15 23:40:14 +0200

    33 done

--- ProjectEuler/module33.py
+++ ProjectEuler/module33.py
@@ -1,19 +1,25 @@
 from module30 import GetNumArray
+from module32 import ConvertArrayOfIntToInt
 
 def WeirdSimplification(nominator, denominator):
-    numArr = GetNumArray(nominator)
+    nomArr = GetNumArray(nominator)
     denArr = GetNumArray(denominator)
 
     repeatedNum = -1
 
-    for num in numArr:
+    for nom in nomArr:
         for den in denArr:
-            if (num == den):
-                repeatedNum = num
+            if (nom == den):
+                repeatedNum = nom
 
-    if (repeatedNum > 0):
-        numArr.remove(repeatedNum)
+    if (repeatedNum > 0 and nomArr.index(repeatedNum) != denArr.index(repeatedNum)):
+        nomArr.remove(repeatedNum)
         denArr.remove(repeatedNum)
+
+    simplNom = ConvertArrayOfIntToInt(nomArr)
+    simplDen = ConvertArrayOfIntToInt(denArr)
+
+    return simplNom, simplDen
 
 
 
@@ -24,5 +30,15 @@ def WeirdSimplification(nominator, denominator):
 
 def SolveProblem():
     for nominator in range(10,100):
-        for denominator in range(10,100):
-            WeirdSimplification(nominator, denominator)
+        for denominator in range(nominator,100):
+            simplNom, simplDen = WeirdSimplification(nominator, denominator)
+
+            if (simplDen == 0):
+                continue
+            if (nominator == simplNom):
+                continue
+            if (denominator == simplDen):
+                continue
+
+            if (float(nominator)/float(denominator) == float(simplNom)/float(simplDen)):
+                print str(nominator) + "/" + str(denominator) + " == " + str(simplNom) + "/" + str(simplDen)
