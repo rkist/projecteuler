b4ad01e0a9721c381740412a77de97a8cbe796dc
Raul Kist <raulkist@gmail.com>
2016-08-17 14:27:21 +0200

    adff

---
+++ ProjectEuler/NumericalHelpers.py
@@ -0,0 +1,19 @@
+from math import *
+
+def GetDivisorsArray(value):
+    arr = []
+    for divisor in range(2,int(sqrt(value) + 1)+1):
+        if (value != divisor) and (value % divisor == 0):
+            arr.append(divisor)
+    return arr
+
+def IsPrime(value):
+    if (value < 0):
+        return False
+    for divisor in range(2,int(sqrt(value) + 1)+1):
+        if (value != divisor) and (value % divisor == 0):
+            return False
+    return True
+
+def Factorial(num):
+    return factorial(num)
---
+++ ProjectEuler/module35.py
@@ -0,0 +1,51 @@
+from ArrayHelpers import *
+from NumericalHelpers import *
+
+
+def GenerateRotations(num):
+    numArr = ConvertIntToIntArray(num)
+    numArrLen = len(numArr)
+
+    resArr = []
+
+    for start in range(0, numArrLen):
+        
+        rotation = []
+        for i in range(start, start + numArrLen):
+            index = i % numArrLen
+            rotation.append(numArr[index])
+
+        retatedNum = ConvertIntArrayToInt(rotation)
+        resArr.append(retatedNum)
+
+    return resArr
+
+
+
+
+
+
+
+def SolveProblem():
+    results = []
+
+    for num in range(2, 1000000):
+        rotations = GenerateRotations(num)
+
+        allPrimes = True
+        for rotation in rotations:
+            allPrimes &= IsPrime(rotation)
+            if(not allPrimes):
+                break
+
+        if (allPrimes):
+            print num
+            results.append(num)
+
+
+
+    print results
+    print len(results)
+
+
+
