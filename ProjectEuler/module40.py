4f8cf2925ea6e0664117834d75ff726d815c3180
Raul Kist <raulkist@gmail.com>
2016-08-19 13:20:09 +0200

    40 done

--- ProjectEuler/module40.py
+++ ProjectEuler/module40.py
@@ -1,9 +1,23 @@
 from ArrayHelpers import *
 from NumericalHelpers import *
 
+def GenerateProblemArray():
+    arr = []
+    arrLen = 0
+    num = 1
+    while (arrLen < 1000001):
+        numArr = ConvertIntToIntArray(num)
+        arr += numArr
+        num += 1
+        arrLen += len(numArr)
+    return arr
+
 
 
 
 
 def SolveProblem():
-    return 0
+    arr = GenerateProblemArray()
+    print len(arr)
+    res = arr[1-1] * arr[10-1] * arr[100-1] * arr[1000-1] * arr[10000-1] * arr[100000-1] * arr[1000000-1]
+    return res
