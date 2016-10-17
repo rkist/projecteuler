489be98634635e46fa0a376d6d32063789f6c412
Raul Kist <raulkist@gmail.com>
2016-10-18 00:03:32 +0200

    54 prog

--- ProjectEuler/module54.py
+++ ProjectEuler/module54.py
@@ -3,9 +3,107 @@ from NumericalHelpers import *
 from FileHelpers import *
 from Parallel import *
 
+class Card:
+    def __init__(self, value, suit):
+        self.Value = value
+        self.Suit = suit
+
+    def __str__(self):
+        return str(self.Value) + str(self.Suit)   
+
+    def __eq__(self, other):
+        return self.Value == other.Value and self.Suit == other.Suit
+
+    def __ne__(self, other):
+        return not self == other
+
+    def __gt__(self, other):
+        return self.Value > other.Value
+
+    def __ge__(self, other):
+        return self.Value >= other.Value
+
+    def __lt__(self, other):
+        return self.Value < other.Value
+
+    def __le__(self, other):
+        return self.Value <= other.Value
+
+    @classmethod
+    def Parse(cls, s):    
+        val = s[:-1]
+        value = 0
+        if (val == 'J'):
+            value = 11
+        elif (val == 'Q'):
+            value = 12
+        elif (val == 'K'):
+            value = 13
+        elif (val == 'A'):
+            value = 14
+        else:
+            value = int(val)
+        suite = s[-1]
+        return cls(value, suite)
+
+
+class Ranks:
+    HighCard = 0 #Highest value card.
+    OnePair = 1 #Two cards of the same value.
+    TwoPairs = 2 #Two different pairs.
+    ThreeOfKind = 3 #Three cards of the same value.
+    Straight = 4 #All cards are consecutive values.
+    Flush = 5 #All cards of the same suit.
+    FullHouse = 6 #Three of a kind and a pair.
+    FourOfKind = 7 #Four cards of the same value.
+    StraightFlush = 8 #All cards are consecutive values of same suit.
+    RoyalFlush = 9 #Ten, Jack, Queen, King, Ace, in same suit.
+
+
+class Hand:
+    def __init__(self, cards):
+        self.Cards = cards
+
+    def __str__(self):
+        s = ""
+        for c in self.Cards:
+             s += str(c) + " "
+        return s
+
+    @classmethod
+    def Parse(cls, s):    
+        cardsStr = s.split(" ")
+        cards = []
+        for cStr in cardsStr:
+            c = Card.Parse(cStr)
+            cards.append(c)
+        return cls(cards)
+
+    def Rank(self):
+        pass
+
+
+
 
 
 
 def SolveProblem():
     print "."   
+    c = Card(12,'A')
+
+    cc = Card.Parse("QA")
+
+    print c
+    print cc
+
+    print c == cc
+
+    h = Hand([c,cc])
+
+    print h
+
+    hh = Hand.Parse("5H 5C 6S 7S KD")
+
+    print hh
+
     return -1
