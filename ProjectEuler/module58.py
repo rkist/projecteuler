abef12cf8bd63e1dff4d8f69276a180963919cd8
Raul Kist <raulkist@gmail.com>
2016-10-24 16:50:15 +0200

    58 porg

--- ProjectEuler/module58.py
+++ ProjectEuler/module58.py
@@ -4,9 +4,41 @@ from FileHelpers import *
 from Parallel import *
 from PrimesCache import *
 
+from module28 import BuildMatrix, PrintMatrix
 
+def CalculatePrimesRatioInDiagnals(matrix):
+    sum = 0
+    pos = [0,0]
+    lenght = len(matrix)
+
+    totalNumbers = 2*lenght - 1
+    totalPrimes = 0
+
+    while (pos[0] < lenght):
+        n = matrix[pos[0]][pos[1]]
+        if (IsPrime(n)):
+            totalPrimes += 1
+        pos[0] += 1
+        pos[1] += 1
+
+    pos = [0, lenght-1]
+    while (pos[0] < lenght):
+        n = matrix[pos[0]][pos[1]]
+        if (IsPrime(n)):
+            totalPrimes += 1
+        pos[0] += 1
+        pos[1] -= 1
+
+    return float(totalPrimes)/float(totalNumbers)
 
 
 def SolveProblem():
-    print "."   
-    return -1
+
+    matrix = BuildMatrix(7)
+    PrintMatrix(matrix)
+
+    ratio = CalculatePrimesRatioInDiagnals(matrix)
+
+    
+
+    return ratio
