61562dac5e556a8559ec320804634d2b236d3297
Raul Kist <Raul.Kist@cegal.com>
2016-11-01 13:25:49 +0100

    63 done 64 start

--- ProjectEuler/module63.py
+++ ProjectEuler/module63.py
@@ -10,5 +10,21 @@ from GraphHelpers import *
 
 def SolveProblem():
     print __name__
+
+    propNumCounter = 0
+
+    for n in range(1000):
+        for p in range(30):
+            num  = n**p
+            numArr = ConvertIntToIntArray(num)
+            numArrLen = len(numArr)
+
+            if (numArrLen == p):
+                propNumCounter += 1
+                print str(n) + "**" + str(p) + " = " + str(num)
+
+            
+
+
        
-    return -1
+    return propNumCounter
---
+++ ProjectEuler/module64.py
@@ -0,0 +1,14 @@
+from ArrayHelpers import *
+from NumericalHelpers import *
+from FileHelpers import *
+from Parallel import *
+from PrimesCache import *
+from GraphHelpers import *
+
+
+
+
+def SolveProblem():
+    print __name__
+       
+    return -1
