e0b5aa6887dc99e6eff654d990e2701b843aecb4
Raul Kist <Raul.Kist@cegal.com>
2018-05-24 13:40:01 +0200

    start 70

--- ProjectEuler/module69.py
+++ ProjectEuler/module69.py
@@ -7,51 +7,26 @@ from GraphHelpers import *
 from Memoize import *
 
 
-
-
 def SolveProblem():
     print __name__
 
-    #maxProportion = 0
-    #maxProportionN = 0
+    maxProportion = 0
+    maxProportionN = 0
 
-    nums = []
-    for n in range(2, 1000001):
-        if IsPrime(n): 
-            continue
-        nums.append(n)
-
-    def phi(n):
-        res = n
-        Nfac = Factor(n)
-        for f in Nfac:
-            res = res*(1 - 1/f)
-        return res
-
-    prior = 0
-    for item in nums:
-        ph = phi(item)
-        val = item/ph
-        if val > prior:
-            prior = val
-            print item, ph, val
-
-
-    #for n in range(2, 10**3+1):
-    #    phi = GetNumberOfSmallerRelativePrimes2(n)
-    #    proportion = float(n)/float(phi)
-    #    #relativePrimes = GetSmallerRelativePrimes(n)
-
-    #    #print ("%s \t %s \t %s \t %s" % (n, phi, proportion, relativePrimes))
-
-    #    if (proportion > maxProportion):
-    #        print ("%s \t %s \t %s" % (n, phi, proportion))
-    #        maxProportion = proportion
-    #        maxProportionN = n
-
-    #    if (n % 1000 == 0):
-    #        print n
-
+    for n in xrange(2, 1000000) :
+        factors = GetUniquePrimeFactors(n)
+        dividend = n
+        divisor = 1
+        for factor in factors :
+            dividend *= factor - 1
+            divisor  *= factor
+        
+        totient = dividend / divisor
+        proportion = float(n) / float(totient)
+        if  maxProportion < proportion :
+            maxProportion = proportion
+            maxProportionN = n
+            print n, totient, proportion
        
     return maxProportionN
 
