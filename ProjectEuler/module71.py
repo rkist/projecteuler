1da97b69754897380a1730a9c4c0a75cf2e5000b
Raul Kist <Raul.Kist@cegal.com>
2018-05-24 16:46:17 +0200

    71 solving to 8 haha

--- ProjectEuler/module71.py
+++ ProjectEuler/module71.py
@@ -6,10 +6,34 @@ from PrimesCache import *
 from GraphHelpers import *
 from Memoize import *
 
+from sortedcollections import SortedDict
+
+LimitN = 8 + 1
+
+def Simplify(n,d):
+    i = 2
+    while(i <= n):
+        if (n % i == 0 and d % i == 0):
+            n /= i
+            d /= i
+        else:
+            i += 1     
+
+    return (n,d)
 
 
 
 def SolveProblem():
     print __name__
+    orderedFractionsDic = SortedDict()
+
+    for d in range(1, LimitN):
+        for n in range(1, d):
+            t = Simplify(n,d)
+            ratio = float(n)/float(d)
+            orderedFractionsDic[ratio] = t
+           
+    for key, val in orderedFractionsDic.iteritems():
+        print val, key
        
     return -1
