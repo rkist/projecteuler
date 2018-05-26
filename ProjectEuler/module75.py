0931cbabc35fc22f3aa7d6a19957f56b64aced68
Raul Kist <raulkist@gmail.com>
2018-05-26 12:05:19 +0200

    ...

--- ProjectEuler/module75.py
+++ ProjectEuler/module75.py
@@ -7,37 +7,10 @@ from GraphHelpers import *
 from Memoize import *
 
 
-def Step(value):
-    valueArr = ConvertIntToIntArray(value)
-    factorialSum = 0
-    for n in valueArr:
-        factorial = Factorial(n)
-        factorialSum += factorial
-
-    return factorialSum
-
-
-def FindLoop(value):
-    return FindLoopRecursion([value])
-
-def FindLoopRecursion(valuesInLoop):
-    newValue = Step(valuesInLoop[-1])
-
-    if (newValue in valuesInLoop):
-        return valuesInLoop
-    
-    valuesInLoop.append(newValue)
-    return FindLoopRecursion(valuesInLoop)
-
 def SolveProblem():
     print __name__
 
     numOfSixtySized = 0
-    for i in range(10**6):
-        loopSize = len(FindLoop(i))
-        if (loopSize == 60):
-            numOfSixtySized += 1
-
 
     return numOfSixtySized
 
