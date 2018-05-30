63f4c5f416ff9b44c09af99f21d1f543a48aabd1
Raul Kist <Raul.Kist@cegal.com>
2018-05-30 11:50:04 +0200

    ...

--- ProjectEuler/module78.py
+++ ProjectEuler/module78.py
@@ -13,20 +13,24 @@ def GenerateSumsCombinations2(number):
         result += GenerateSumsCombinationRecursion2(number, n, n)
     return result
 
+combinationsCache = {}
 def GenerateSumsCombinationRecursion2(finalSum, partialSum, lastAddedValue):
-    if (partialSum == finalSum):
-        return 1
+    k = (finalSum, partialSum, lastAddedValue)
+    if (not combinationsCache.has_key(k)):
+        sums = 0
+        if (partialSum == finalSum):
+            sums = 1
+        else:            
+            for n in xrange(lastAddedValue, finalSum - partialSum + 1):
+                sums += GenerateSumsCombinationRecursion2(finalSum, partialSum + n, n)
 
-    sums = 0
-    for n in range(lastAddedValue, finalSum - partialSum + 1):
-        sums += GenerateSumsCombinationRecursion2(finalSum, partialSum + n, n)
-    return sums
-
+        combinationsCache[k] = sums
+    return combinationsCache[k]
 
 def SolveProblem():
     print __name__
 
-    number = 5
+    number = 1
     sums = GenerateSumsCombinations2(number)
     while (sums % 1000000 != 0):
         number += 1 
