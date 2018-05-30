b6eb283643bf849e7f7df8bd7989fcc33d0308dc
Raul Kist <raulkist@gmail.com>
2018-05-30 22:39:21 +0200

    ....

--- ProjectEuler/module79.py
+++ ProjectEuler/module79.py
@@ -7,9 +7,27 @@ from GraphHelpers import *
 from Memoize import *
 
 
+def GenPassword(passwdsEntries):
+    passwords = []
+    for entry in passwdsEntries:
+        if (len(passwords) == 0):
+            passwords.append([entry[0], entry[1]])
+
+    return passwords
+
 
 
 def SolveProblem():
     print __name__
+
+    pwds = ReadCardsFile("input/input79.txt")
+    passwdsEntries = []
+    for pwd in pwds:
+        passwdsEntries.append((pwd[0], pwd[1]))
+        passwdsEntries.append((pwd[1], pwd[2]))
+        passwdsEntries.append((pwd[0], pwd[2]))
        
+    passwd = GenPassword(passwdsEntries)
+    print passwd
+
     return -1
